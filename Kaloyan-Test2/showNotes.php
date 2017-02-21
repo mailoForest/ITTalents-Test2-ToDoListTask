6dcd48e970700188816c2be842e402b79c48015b
Kaloyan Petrov <kaloyanpetrov1994@gmail.com>
2017-02-21 14:01:13 +0200

    Add files via upload

---
+++ Kaloyan-Test2/showNotes.php
@@ -0,0 +1,40 @@
+<h2>Your notes ordered:</h2>
+<table>
+    <?php
+    $notes = [];
+
+    for ($i = 0; $i <= 5; $i++){
+        $notes[] = [];
+    }
+    $getPriority = 0;
+    $longestListOfPriorities = 0;
+    $handle = fopen('notes.txt','r');
+        while (!feof($handle)){
+            $line = fgets($handle);
+            $getPriority = strstr($line, '#');
+            $getPriority = str_replace('#', '', $getPriority);
+            $getPriority += 0;
+
+            $notes[$getPriority][] = $line;
+        }
+
+//        foreach ($notes as $note){
+//            $countNotes = count($note);
+//            if ($longestListOfPriorities < $countNotes){
+//                $longestListOfPriorities = $countNotes;
+//            }
+//        }
+    ?>
+
+    <tr><th>Priority</th><th>Text</th></tr>
+    <?php
+        for ($i = 0; $i <= 5; $i++){
+            echo "<tr><td>$i</td>";
+            echo "<td>". $notes[$i] ."</td></tr>";
+        }
+    ?>
+<?php
+    fclose($handle);
+    var_dump($notes);
+?>
+</table>
